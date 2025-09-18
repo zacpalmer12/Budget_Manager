e435c5e96009395603a609c8ea725069dc3a39f9
Zac Palmer <zpalmer72@gmail.com>
2025-09-18 18:09:46 +0100

    more usability

--- src/budget/Main.java
+++ src/budget/Main.java
@@ -7,9 +7,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double totalIncome = 0;
-        double totalExpenses = 0;
-        List<String> list = new ArrayList<>();
+
+        double balance = 0;
+        double foodTotal = 0, clothesTotal = 0, entertainmentTotal = 0, otherTotal = 0;
+
+        List<String> food = new ArrayList<>();
+        List<String> clothes = new ArrayList<>();
+        List<String> entertainment = new ArrayList<>();
+        List<String> other = new ArrayList<>();
+        List<String> all = new ArrayList<>();
 
         outerLoop:
         while (true) {
@@ -28,48 +34,188 @@ public class Main {
                 case "1":
                     System.out.println("Enter income:");
                     double income = sc.nextDouble();
-                    totalIncome += income;
-                    System.out.println("Income was added!\n");
+                    balance += income;
+                    System.out.println("Income was added!");
+                    System.out.println();
                     break;
 
                 case "2":
-                    System.out.println("Enter purchase name:");
-                    sc.nextLine(); // consume leftover newline
-                    String purchaseName = sc.nextLine();
-                    System.out.println("Enter its price:");
-                    double purchasePrice = sc.nextDouble();
-                    totalExpenses += purchasePrice;
-                    list.add(purchaseName + " $" + String.format("%.2f", purchasePrice));
-                    System.out.println("Purchase was added!\n");
+                    purchaseLoop:
+                    while (true) {
+                        System.out.println("""
+                                Choose the type of purchase
+                                1) Food
+                                2) Clothes
+                                3) Entertainment
+                                4) Other
+                                5) Back
+                                """);
+
+                        String type = sc.next();
+                        sc.nextLine(); // clear newline
+
+                        switch (type) {
+                            case "1":
+                                System.out.println("Enter purchase name:");
+                                String foodName = sc.nextLine().trim();
+                                System.out.println("Enter purchase price:");
+                                double foodPrice = Double.parseDouble(sc.nextLine().trim());
+
+                                String foodEntry = foodName + " $" + String.format("%.2f", foodPrice);
+                                food.add(foodEntry);
+                                all.add(foodEntry);
+                                foodTotal += foodPrice;
+
+                                System.out.println("Purchase was added!");
+                                System.out.println();
+                                break;
+
+                            case "2":
+                                System.out.println("Enter purchase name:");
+                                String clothesName = sc.nextLine().trim();
+                                System.out.println("Enter purchase price:");
+                                double clothesPrice = Double.parseDouble(sc.nextLine().trim());
+
+                                String clothesEntry = clothesName + " $" + String.format("%.2f", clothesPrice);
+                                clothes.add(clothesEntry);
+                                all.add(clothesEntry);
+                                clothesTotal += clothesPrice;
+
+                                System.out.println("Purchase was added!");
+                                System.out.println();
+                                break;
+
+                            case "3":
+                                System.out.println("Enter purchase name:");
+                                String entertainmentName = sc.nextLine().trim();
+                                System.out.println("Enter purchase price:");
+                                double entertainmentPrice = Double.parseDouble(sc.nextLine().trim());
+
+                                String entertainmentEntry = entertainmentName + " $" + String.format("%.2f", entertainmentPrice);
+                                entertainment.add(entertainmentEntry);
+                                all.add(entertainmentEntry);
+                                entertainmentTotal += entertainmentPrice;
+
+                                System.out.println("Purchase was added!");
+                                System.out.println();
+                                break;
+
+                            case "4":
+                                System.out.println("Enter purchase name:");
+                                String otherName = sc.nextLine().trim();
+                                System.out.println("Enter purchase price:");
+                                double otherPrice = Double.parseDouble(sc.nextLine().trim());
+
+                                String otherEntry = otherName + " $" + String.format("%.2f", otherPrice);
+                                other.add(otherEntry);
+                                all.add(otherEntry);
+                                otherTotal += otherPrice;
+
+                                System.out.println("Purchase was added!");
+                                System.out.println();
+                                break;
+
+                            case "5":
+                                break purchaseLoop;
+                        }
+                    }
                     break;
 
                 case "3":
-                    if (list.isEmpty()) {
-                        System.out.println("The purchase list is empty.\n");
-                    } else {
-                        for (String s : list) {
-                            System.out.println(s);
+                    while (true) {
+                        System.out.println("""
+                                Choose the type of purchase
+                                1) Food
+                                2) Clothes
+                                3) Entertainment
+                                4) Other
+                                5) All
+                                6) Back
+                                """);
+
+                        String type = sc.next();
+
+                        switch (type) {
+                            case "1":
+                                System.out.println("Food:");
+                                if (food.isEmpty()) {
+                                    System.out.println("The purchase list is empty!");
+                                } else {
+                                    for (String item : food) System.out.println(item);
+                                    System.out.println("Total sum: $" + String.format("%.2f", foodTotal));
+                                }
+                                System.out.println();
+                                break;
+
+                            case "2":
+                                System.out.println("Clothes:");
+                                if (clothes.isEmpty()) {
+                                    System.out.println("The purchase list is empty!");
+                                } else {
+                                    for (String item : clothes) System.out.println(item);
+                                    System.out.println("Total sum: $" + String.format("%.2f", clothesTotal));
+                                }
+                                System.out.println();
+                                break;
+
+                            case "3":
+                                System.out.println("Entertainment:");
+                                if (entertainment.isEmpty()) {
+                                    System.out.println("The purchase list is empty!");
+                                } else {
+                                    for (String item : entertainment) System.out.println(item);
+                                    System.out.println("Total sum: $" + String.format("%.2f", entertainmentTotal));
+                                }
+                                System.out.println();
+                                break;
+
+                            case "4":
+                                System.out.println("Other:");
+                                if (other.isEmpty()) {
+                                    System.out.println("The purchase list is empty!");
+                                } else {
+                                    for (String item : other) System.out.println(item);
+                                    System.out.println("Total sum: $" + String.format("%.2f", otherTotal));
+                                }
+                                System.out.println();
+                                break;
+
+                            case "5":
+                                System.out.println("All:");
+                                if (all.isEmpty()) {
+                                    System.out.println("The purchase list is empty!");
+                                } else {
+                                    for (String item : all) System.out.println(item);
+                                    double total = foodTotal + clothesTotal + entertainmentTotal + otherTotal;
+                                    System.out.println("Total sum: $" + String.format("%.2f", total));
+                                }
+                                System.out.println();
+                                break;
+
+                            case "6":
+                                break;
                         }
-                        System.out.println("Total sum: $" + String.format("%.2f", totalExpenses)+"\n");
+
+                        if (type.equals("6")) break;
                     }
                     break;
 
                 case "4":
-                    double balance = totalIncome - totalExpenses;
-                    if (balance < 0) balance = 0;
-                    System.out.println("Balance: $" + String.format("%.2f", balance)+"\n");
+                    double spent = foodTotal + clothesTotal + entertainmentTotal + otherTotal;
+                    System.out.println("Balance: $" + String.format("%.2f", balance - spent));
+                    System.out.println();
                     break;
 
                 case "0":
+                    System.out.println("Bye!");
+                    sc.close();
                     break outerLoop;
 
                 default:
-                    System.out.println("Unknown option, try again.\n");
+                    System.out.println("Unknown option, try again.");
+                    System.out.println();
                     break;
             }
         }
-
-        System.out.println("Bye!");
-        sc.close();
     }
 }
